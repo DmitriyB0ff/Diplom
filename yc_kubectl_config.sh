aaee9fe6606a1baffdc5f466bb6b1be5b6d708db
Куропатко Дмитрий <dmitry.kuropatko@yandex.ru>
2024-11-13 11:59:35 +0500

    Test

--- yc_kubectl_config.sh
+++ yc_kubectl_config.sh
@@ -5,12 +5,15 @@ curl https://storage.yandexcloud.net/yandexcloud-yc/install.sh |     bash -s -- 
 #Переходим в каталог ./yc/bin
 cd ./yc/bin/
 #Задаём конфигурацию профиля sa
+echo "TOKEN"
 ./yc config set token ${YC_SA_KEY} #service-account-key ${YC_SA_KEY}
+echo "CLOUD"
 ./yc config set cloud-id ${YC_CLOUD_ID}
 ./yc config set folder-id ${YC_FOLDER_ID}
 #Проверяем видимость кубекластера в YC
 ./yc managed-kubernetes cluster list
 #Конфигурируем kubeconfig
+echo "YC_CLUSTER_ID"
 ./yc managed-kubernetes cluster get-credentials ${YC_CLUSTER_ID} --external
 #Добавляем manespace в kubeconfig
 sed -i '9i\    namespace: momo-app' /root/.kube/config
